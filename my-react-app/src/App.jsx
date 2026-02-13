e9522f2f6668399dc9f5d637cdc6fce6cf565beb
Gaurav Chaudhari <gauravchaudhari232004@gmail.com>
2026-02-14 02:30:29 +0530

    fix case issue

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 import './App.css'
-import Portfolio from './Temp'
+import Portfolio from './portfolio'
 
 function App() {
   const [count, setCount] = useState(0)
